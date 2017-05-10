9e03b912ade2a9b257dabab1a8ffb96341ad3b8b
Marco Borja <marcoabr93@gmail.com>
2017-05-10 17:13:28 +0200

    Estructura

---
+++ src/org/company/Controller/ProductosApp.java
@@ -0,0 +1,7 @@
+package org.company.Controller;
+
+/**
+ * Created by marco on 10/5/17.
+ */
+public class ProductosApp {
+}
---
+++ src/org/company/Model/Producto.java
@@ -0,0 +1,7 @@
+package org.company.Model;
+
+/**
+ * Created by marco on 10/5/17.
+ */
+public class Producto {
+}
